a81d68b502cdaae34695abe0ff61ba566fdb06f3
GYMK <gymk.tech@gmail.com>
2018-01-13 21:06:44 +0900

    fixed version2 grep utility

--- the_python_book/utilities/my_grep_v2.py
+++ the_python_book/utilities/my_grep_v2.py
@@ -7,32 +7,48 @@ Using this as reference
 '''
 
 
-import sys
+import sys as sys
 
-# read over all the files
-# in each file reads line by line and yields thosee line
-def catFiles(fileNames):
-    for f in fileNames:
-        for line in open(f):
-            yield(line)
+def cat_files(filenames):
+    """
+    read over all the files
+    in each file reads line by line and yields thosee line
+        :param filenames: an iterator to list of file names
+    """
+    print('--->catFiles {0}'.format(type(filenames)))
+    for file in filenames:
+        for line in open(file):
+            yield line
 
-# returns a generator over lines object which yields if pattern is found in lines object
-def grep(pattern, lines):
-    return (line for line in lines if pattern in line)
+def grep(pattern, filenames):
+    """
+    another version for test
+        :param pattern: pattern to be searched over the files
+        :param filenames: list of filenames
+    """
+    return (line for line in cat_files(filenames) if pattern in line)
 
-# function which iterates over lines object and prints it content
-def printLines(lines):
+def print_lines(lines):
+    """
+    function which iterates over lines object and prints it content
+        :param lines:  an iterator over lines for printing
+    """
+    print('--->print_lines {0}'.format(type(lines)))
     for line in lines:
         print(line)
 
-# Entry function for this python script
 def start():
+    """
+    Entry function for this python script
+    """
     pattern = sys.argv[1]
-    fileNames = sys.argv[2:]
-    lines = grep(pattern, fileNames)
-    #lines = catFiles(fileNames)
-    printLines(lines)
-    pass
+    filenames = list(sys.argv[2:])
+    lines = grep(pattern, filenames)
+    print('--->start {0}'.format(type(lines)))
+    print_lines(lines)
+    #lines = cat_files(filenames)
+    #print(type(lines))
+    #print_lines(lines)
 
 if __name__ == '__main__':
     start()
