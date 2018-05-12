967a9e0e0fe862431b19467c3a8c5faa0b8d2881
GYMK <gymk.tech@gmail.com>
2018-05-12 16:44:01 +0900

    PyPlot updated

--- Others/rand_data.cpp
+++ Others/rand_data.cpp
@@ -11,6 +11,8 @@ int main(int argc, char * argv[])
 	int v;
 	ofstream out(argv[1]);
 
+	cout << "Seed: " << seed << " count: " << count << " OutputFile: " << argv[1] << endl;
+
 	srand(seed);
 
 	for(int i = 0; i < count; i++)
